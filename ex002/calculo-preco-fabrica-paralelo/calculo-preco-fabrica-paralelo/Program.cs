7816b1297fefa87e2b70bcf88f561af22a8a2550
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-04-22 14:53:14 -0300

    cálculo de preço de fábrica paralelizado

---
+++ ex002/calculo-preco-fabrica-paralelo/calculo-preco-fabrica-paralelo/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace calculo_preco_fabrica_paralelo
+{
+    internal class Program
+    {
+        private static double val_ipi;
+        private static double val_distr;
+        static void Main(string[] args)
+        {
+            double prc_fabr, prc_total;
+            Console.Write("Informe o preço de fábrica: ");
+            prc_fabr = double.Parse(Console.ReadLine());
+
+            var t1 = new Thread(() => calc_ipi(prc_fabr));
+            var t2 = new Thread(() => calc_distr(prc_fabr));
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+            prc_total = prc_fabr + val_ipi + val_distr;
+
+            Console.WriteLine("O valor total do veículo é de R$" + Math.Round(prc_total, 2));
+            Console.WriteLine("O valor ao distribuidor é de  R$" + Math.Round(val_distr, 2));
+            Console.WriteLine("O valor relativo ao IPI é de  R$" + Math.Round(val_ipi, 2));
+        }
+        private static void calc_ipi(double prc_fabr)
+        {
+            if (prc_fabr <= 200000.00)
+            {
+                val_ipi = 0.00;
+            }
+            else if (prc_fabr <= 400000.00)
+            {
+                val_ipi = prc_fabr * 0.15;
+            }
+            else
+            {
+                val_ipi = prc_fabr * 0.20;
+            }
+        }
+
+        private static void calc_distr(double prc_fabr)
+        {
+            if (prc_fabr <= 150000.00)
+            {
+                val_distr = prc_fabr * 0.03;
+            }
+            else if (prc_fabr <= 350000.00)
+            {
+                val_distr = prc_fabr * 0.08;
+            }
+            else
+            {
+                val_distr = prc_fabr * 0.12;
+            }
+        }
+    }
+}
---
+++ ex002/calculo-preco-fabrica-paralelo/calculo-preco-fabrica-paralelo/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("calculo-preco-fabrica-paralelo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("calculo-preco-fabrica-paralelo")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dd5bf3f3-0f27-4757-87e4-2f9e74433327")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
